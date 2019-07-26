c610607461edf8bc70eb9ec1ba0650f8291f75fb
Dwight Millar <millar.dwight@gmail.com>
2019-07-26 09:47:41 -0700

    added admin and students states for App, different views for each

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,15 +3,26 @@ import Video from './video.jsx';
 import SidePanel from './sidepanel.jsx';
 
 class App extends React.Component{
-    constructor(){
-        super()
-        this.state={
+    constructor(props){
+        super(props);
+        this.state = {
             UserType: 'admin'
+        };
+        this.switchUser = this.switchUser.bind(this);
+    }
+
+    switchUser(){
+        if (this.state.UserType === 'admin'){
+            this.setState({ UserType: 'student' })
+        } else {
+            this.setState({ UserType: 'admin' })
         }
+
     }
     render(){
         return(
             <div id="app">
+                <button onClick={this.switchUser}></button>
                 <Video UserType={this.state.UserType}/>
                 <SidePanel UserType={this.state.UserType}/>
             </div>
--- client/components/video.jsx
+++ client/components/video.jsx
@@ -1,43 +1,80 @@
 import React from 'react';
 
 class Video extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            UserType: 'admin'
+        }
+    }
+
+    componentDidUpdate(){
+        console.log('video state updated')
+    }
+
     render(){
-        return(
-         <div id="video">
-             <div id="twitch-embed" style={{'height': 80 + 'vh', 'width': 70 + 'vw', 'top': 40 + 'vh'}}>
-             <iframe
-                src="https://player.twitch.tv/?channel=dallas&muted=true"
-                height="100%"
-                width="100%"
-                frameBorder="0"
-                scrolling="no"
-                allowFullScreen={true}
-                style={{'position':'relative',
-                        'top':7 + 'vh'}}>
-             </iframe>
-                    <button className="button button4"
-                        style={{
-                            'position': 'absolute',
-                            'top': 50 + '%',
-                            'right': 96 + '%',
-                            'backgroundColor': '#4CAF50',
-                            'display': 'inline-block',
-                            'fontSize': 16 + 'px'
-                        }}>click me
+        console.log('video state updated')
+
+
+        if(this.props.UserType === 'admin'){
+            return (
+                <div id="video">
+                    <div id="twitch-embed" style={{ 'height': 80 + 'vh', 'width': 70 + 'vw', 'top': 40 + 'vh' }}>
+                        <iframe
+                            src="https://player.twitch.tv/?channel=dallas&muted=true"
+                            height="100%"
+                            width="100%"
+                            frameBorder="0"
+                            scrolling="no"
+                            allowFullScreen={true}
+                            style={{
+                                'position': 'relative',
+                                'top': 7 + 'vh'
+                            }}>
+                        </iframe>
+                        <button className="button button4"
+                            style={{
+                                'position': 'absolute',
+                                'top': 50 + '%',
+                                'right': 96 + '%',
+                                'backgroundColor': '#4CAF50',
+                                'display': 'inline-block',
+                                'fontSize': 16 + 'px'
+                            }}>click me
                 </button>
 
-                <button className="button button4"
-                        style={{
-                            'position': 'absolute',
-                            'top': 40 + '%',
-                            'right': 96 + '%',
-                            'backgroundColor': '#4CAF50',
-                            'display': 'inline-block',
-                            'fontSize': 16 + 'px'
-                        }}>click me
+                        <button className="button button4"
+                            style={{
+                                'position': 'absolute',
+                                'top': 40 + '%',
+                                'right': 96 + '%',
+                                'backgroundColor': '#4CAF50',
+                                'display': 'inline-block',
+                                'fontSize': 16 + 'px'
+                            }}>click me
                 </button>
-            </div>
-        </div>)
+                    </div>
+                </div>)
+        } else {
+            return (
+                <div id="video">
+                    <div id="twitch-embed" style={{ 'height': 80 + 'vh', 'width': 70 + 'vw', 'top': 40 + 'vh' }}>
+                        <iframe
+                            src="https://player.twitch.tv/?channel=dallas&muted=true"
+                            height="100%"
+                            width="100%"
+                            frameBorder="0"
+                            scrolling="no"
+                            allowFullScreen={true}
+                            style={{
+                                'position': 'relative',
+                                'top': 7 + 'vh'
+                            }}>
+                        </iframe>
+                    </div>
+                </div>)
+        }
+
     }
 }
 export default Video;
