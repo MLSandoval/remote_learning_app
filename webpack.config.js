dd628a2e87fa8ae7778aa57d26aea24f71b4796d
Dwight Millar <millar.dwight@gmail.com>
2019-07-30 13:08:22 -0700

    changed port to 3001

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   devtool: 'source-map',
   devServer: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 3001,
     contentBase: publicPath,
     watchContentBase: true,
     stats: 'minimal',
