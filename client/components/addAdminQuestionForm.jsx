a5af7d89ee90582af6b31476bb1cb65ed6de9e8c
Dwight Millar <millar.dwight@gmail.com>
2019-08-07 18:04:29 -0700

    added admin/student options for login, question author name reflects input username

--- client/components/addAdminQuestionForm.jsx
+++ client/components/addAdminQuestionForm.jsx
@@ -112,7 +112,7 @@ export default class AddAdminQuestionForm extends React.Component{
         console.log("new value ", this.state.selectedAnswer);
         if(this.props.view === 'add'){
             return(
-                <div className="modal bringModalForward centerOnVideo" tabIndex="-1" role="dialog">
+                <div className="modal " tabIndex="-1" role="dialog">
                     <div className="modal-dialog modal-lg" role="document">
                         <div className="modal-content">
                             <div className="modal-header">
