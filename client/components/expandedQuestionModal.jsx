26589b0f44789c4663f739e54e3178d878d7b2f8
amac2d <amac2d@student.mtsac.edu>
2019-08-08 18:48:26 +0000

    actually saved

--- client/components/expandedQuestionModal.jsx
+++ client/components/expandedQuestionModal.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
 export default  class ExpandedQuestionModal extends React.Component {
     constructor(props){
         super(props);
@@ -8,14 +7,6 @@ export default  class ExpandedQuestionModal extends React.Component {
         this.handleDeleteStudentQuestion = this.handleDeleteStudentQuestion.bind(this);
         this.handleChildClick = this.handleChildClick.bind(this);
     }
-=======
-export default class ExpandedQuestionModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggleView = this.toggleView.bind(this);
-    this.handleDeleteStudentQuestion = this.handleDeleteStudentQuestion.bind(this);
-  }
->>>>>>> 5ed7185dafdedc0b3c5cc5b22cf722a9a0dbd18e
 
   toggleView() {
     this.props.resetSelectedQuestion();
@@ -28,7 +19,6 @@ export default class ExpandedQuestionModal extends React.Component {
     event.stopPropagation()
     };
 
-<<<<<<< HEAD
     render(){
         console.log("ExpandedQuestionModal: ", this.props.questionTarget)
 
@@ -55,32 +45,4 @@ export default class ExpandedQuestionModal extends React.Component {
             </div>)
 
     }
-=======
-  render() {
-    console.log("ExpandedQuestionModal: ", this.props.questionTarget)
-
-
-    return (
-      <div className="modal" tabIndex="-1" role="dialog" onClick={this.toggleView}>
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">{this.props.questionTarget.author}</h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true" onClick={this.toggleView}>&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              <p>{this.props.questionTarget.question}</p>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={this.handleDeleteStudentQuestion}>Delete</button>
-              <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={this.toggleView}>Close</button>
-            </div>
-          </div>
-        </div>
-      </div>)
-
-  }
->>>>>>> 5ed7185dafdedc0b3c5cc5b22cf722a9a0dbd18e
 }
