6f26923147be6aa29cc47620eea1144bd8bd8278
michael sandoval <michaelsandoval8@gmail.com>
2019-10-22 19:50:04 +0000

    refactored backend to solve socket problem, and be more organized

--- server/index.js
+++ server/index.js
@@ -1,194 +1,200 @@
-//requires
+//There isn't a section of code I am particularly proud of,
+// backend scripts are fairly straightforward and are achieved through 
+// good diligence.
+
+//However, I am proud of the organization of this script, and the use
+// of socket.io.
+
 const express = require('express');
 const server = express();
 const cors = require('cors');
+const ServerError = require('./error');
 const path = require('path');
 const mysql = require('mysql');
-const creds = require('./mysql_credentials.js');
-const db = mysql.createConnection(creds);
+const creds = require('./mysql_credentials');
+const db = mysql.createPool(creds);
 const pubDirectory = path.join(__dirname, '/public');
-
-//socket.IO
+const bodyParser = require('body-parser');
 const http = require('http').createServer(server);
 const io = require('socket.io')(http);
-
-//server uses
+server.use(bodyParser.json());
 server.use(cors());
-server.use(express.urlencoded({ extended: false }));
 server.use(express.static(pubDirectory));
 server.use(express.json());
 
-
-// endpoint to get student questions
-server.get('/getStudentsQuestions',(request, response) => {
-  db.connect(function () {
-    const query = `SELECT questionsQueue.id, questionsQueue.question, questionsQueue.studentUser_id as author
-                   FROM questionsQueue
-                   `;
-    db.query(query, function (error, data) {
-      if (!error) {
-        response.send({
-          success: true,
-          data
-        });
-      }
-    });
-  });
-});
-
-// endpoint to get admin questions
-server.get('/getAdminQuestions',(req, res) => {
-  db.connect(function () {
-    const query = `SELECT q.id, q.question, q.questionOwner_id as admin_id, GROUP_CONCAt(a.id) AS answer_ids, GROUP_CONCAT(a.answer) as answers
-                  FROM questionsAdmin as q
-                  JOIN answerOptions as a
-                  ON q.id = a.question_id
-                  WHERE q.questionOwner_id = 1
-                  GROUP BY q.id`;
-    db.query(query, function (error, data) {
-      if (!error) {
-        res.send({
-          success: true,
-          data
-        });
-      }
-    });
-  });
-});
-
-// endpoint to delete admin question by id
-server.delete('/adminQuestion',(req, res) => {
-  db.connect(function () {
-    const adminQuestionID = parseInt(req.query.adminQuestionID);
-    const query = `DELETE answerOptions, questionsAdmin
-                   FROM answerOptions
-                   JOIN questionsAdmin ON questionsAdmin.id = answerOptions.question_id
-                   WHERE questionsAdmin.id = ${adminQuestionID}`
-    db.query(query, function (error, data) {
-      if (!error) {
-        const output = {
-          success: true,
-          data
-        };
-        res.send( output );
-      }
-    });
-  });
-});
-
-server.post('/addAdminQuestion', (req,res)=>{
-    let {correctAnswer, adminID, question} = req.body;
-    let ansArray = req.body.answers.split(',');
-    let [ans0, ans1, ans2, ans3] = ansArray;
-    let regex = /(['])/g;
-    //this regex stuff is to accommodate apostraphes into the mysql queries
-    for(let index = 0; index > 4; i++){
-      switch(index){
-        case 0: ans0 = ans0.search(regex).replace("/'");
-          break;
-        case 1: ans1 = ans1.search(regex).replace("/'");
-          break;
-        case 2: ans2 = ans2.search(regex).replace("/'");
-          break;
-        case 3: ans3 = ans3.search(regex).replace("/'");
-          break;
-      }  
+server.get('/students-questions', (req, res, next) => {
+  const query = `
+    SELECT questionsQueue.id, questionsQueue.question, questionsQueue.studentUserId as author
+      FROM questionsQueue`;
+  db.query(query, (error, data) => {
+    if (error) {
+      console.error(error);
+      return next(new ServerError('Internal server error.'), 500);
     }
-    
-    // this^ is meant to accommodate for apostrophes in the question text, but
-    // problematic code, gives type error even though im checking that question variable is 
-    // a string before calling .search().replace() like i did on the answer strings 
-    // question = question.toString().search(regex).replace("/'");
-    
-
-    let questionID;
-    let firstAnswerID;
-    let insertQuestionQuery = `
-        INSERT INTO questionsAdmin ( question, questionOwner_id, correctAnswer )
-            VALUES
-            ('${question}', ${adminID}, '${correctAnswer}')
-        `;
-    db.query(insertQuestionQuery, (error, results, fields)=>{
-        if (error) {
-            console.error(error);
-            process.exit(1);
-        }
-        questionID = results.insertId;
-        let insertAnswersQuery = `
-          INSERT INTO answerOptions ( question_id, answer )
-            VALUES (${questionID}, "${ans0}"),
-                (${questionID}, "${ans1}"),
-                (${questionID}, "${ans2}"),
-                (${questionID}, "${ans3}")
-        `;
-        db.query(insertAnswersQuery, (error, results, fields) => {
-            if (error) {
-                console.error(error);
-                process.exit(1);
-            }
-            firstAnswerID = results.insertId;
-            res.send({success: 'true', questionID, firstAnswerID});
-        });
-    })
-});
-
-server.post('/addQuestionQ', (req, res) => {
-    let { studentUsername, question } = req.body;
-    let insertQuestionQQuery = `
-        INSERT INTO questionsQueue ( question, studentUser_id)
-            VALUES
-            ('${question}', '${studentUsername}')
-        `;
-    db.query(insertQuestionQQuery, (error, results, fields) => {
-        if (error) {
-            console.error(error);
-            process.exit(1);
-        }
-        res.send({success: true, data:{newID: results.insertId}});
-    })
-});
-
-server.delete('/studentQuestion', (req, res) => {
-  db.connect(function () {
-    const { studentQuestionID } = req.query;
-    let query = 'DELETE FROM ?? WHERE ?? = ?';
-    let inserts = ['questionsQueue', 'id', studentQuestionID];
-    let sql = mysql.format(query, inserts);
-    db.query(sql, (error, data) =>  {
-      if (!error) {
-        const output = {
-          success: true,
-          data
-        };
-        res.send( output );
-      }
+    res.send({
+      success: true,
+      data
     });
   });
 });
 
-//socketio listeners
-server.get('/', (req,res)=>{
-  res.send({succes: true, message: 'socketIO listener success'});
+server.get('/admin-questions', (req, res, next) => {
+  const { adminID } = req.body || 1;
+  if (!adminID) return next(new ServerError('Must provide admin ID', 400));
+  const query =
+    `SELECT q.id, q.question, q.questionOwnerId as adminId, GROUP_CONCAt(a.id) AS answerIds, GROUP_CONCAT(a.answer) as answers
+      FROM questionsAdmin as q
+      JOIN answersOptions as a
+      ON q.id = a.questionId
+      WHERE q.questionOwnerId = ?
+      GROUP BY q.id`;
+  const params = [adminID];
+
+  db.query(query, params, (error, data) => {
+    if (error) {
+      console.error(error);
+      return next(new ServerError('Internal server error.', 500));
+    }
+    const output = {
+      success: true,
+      data: output
+    };
+    res.send(output);
+  });
 });
 
-io.on('connection',(socket)=> {console.log('a user has connected!')});
+server.delete('/admin-question', (req, res, next) => {
+  let adminQuestionID = req.query.adminQuestionID;
+  if (!adminQuestionID) return next(new ServerError('Must provide question ID for deletion.', 400));
+  adminQuestionID = parseInt(adminQuestionID);
+  if (Number.isNaN(adminQuestionID)) return next(new ServerError('Admin question ID must be a number.', 400));
+  const query =
+    `DELETE answerOptions, questionsAdmin
+      FROM answerOptions
+      JOIN questionsAdmin ON questionsAdmin.id = answerOptions.questionId
+      WHERE questionsAdmin.id = ?`;
+  let params = [adminQuestionID];
 
-//listen for broadcast question, send it back to front end to reach all listeners
-io.on('connection', (socket) => {
-  socket.on('broadcast', (question) => {
-    console.log('backend socket received. question data: ', question);
-    io.emit('questionToBroadcast', question);
+  db.query(query, params, (error, data) => {
+    if (error) {
+      console.error(error);
+      return next(new ServerError('Internal server error.', 500));
+    }
+    const output = {
+      success: true,
+      data
+    };
+    res.send(output);
   });
+});
+
+server.post('/admin-question', (req, res, next) => {
+  if (!req.body.correctAnswer) return next(new ServerError('Must select correct answer.', 400));
+  if (!req.body.adminID) return next(new ServerError('Must provide admin ID.', 400));
+  if (!req.body.question) return next(new ServerError('Must provide question text.', 400));
+  if (!req.body.answers) return next(new ServerError('Must provide 4 answers options.', 400));
+  const { correctAnswer, adminID, question } = req.body;
+  const ansArray = req.body.answers.split(',');
+  const [ans0, ans1, ans2, ans3] = ansArray;
+  let questionID;
+  const query1 = `
+    INSERT INTO questionsAdmin ( question, questionOwnerId, correctAnswer )
+      VALUES (?, ?, ?)`;
+  const params1 = [question, adminID, correctAnswer];
+
+  db.query(query1, params1, (error, data) => {
+    if (error) {
+      console.error(error);
+      next(new ServerError('Internal server error.', 500));
+    }
+    questionID = data.insertId;
+    const query2 = `
+      INSERT INTO answerOptions ( question_id, answer )
+        VALUES (?, ?),
+          (?, ?),
+          (?, ?),
+          (?, ?)`;
+    const params2 = [questionID, ans0, questionID, ans1, questionID, ans2, questionID, ans3];
+
+    db.query(query2, params2, (error, data) => {
+      if (error) {
+        console.error(error);
+        next(new ServerError('Internal server error.', 500));
+      }
+      const firstAnswerID = data.insertId;
+      const output = {
+        success: true,
+        questionID,
+        firstAnswerID
+      };
+      res.send(output);
+    });
+  })
+});
+
+server.post('/student-question', (req, res, next) => {
+  const { studentUsername, question } = req.body;
+  if (!studentUsername) return next(new ServerError('Must provide student username.', 400));
+  if (!question) return next(new ServerError('Must provide question.', 400));
+  const query = `
+    INSERT INTO questionsQueue ( question, studentUser_id)
+      VALUES
+      (?, ?)`;
+  const params = [question, studentUsername];
+
+  db.query(query, params, (error, data) => {
+    if (error) {
+      console.error(error);
+      next(new ServerError('Internal server error.', 400));
+    }
+    const output = {
+      success: true,
+      data: {
+        questionID: data.insertId
+      }
+    };
+    res.send(output);
+  })
+});
+
+server.delete('/student-question', (req, res, next) => {
+  const { studentQuestionID } = req.query;
+  if (!studentQuestionID) return next(new ServerError('Must provide a question ID.', 400));
+  const query = `
+    DELETE FROM questionsQueue 
+      WHERE id = ?`;
+  const params = [studentQuestionID];
+
+  db.query(query, params, (error, data) => {
+    if (error) {
+      console.error(error);
+      return new ServerError('Internal server error.', 500);
+    }
+    const output = {
+      success: true,
+      data
+    };
+    res.send(output);
+  });
+});
+
+//socket.io block
+io.on('connection', (socket) => { console.log('A client has connected!') });
+
+server.post('/broadcast', (req, res, next) => {
+  const question = req.body.question;
+  if (!question) return new ServerError('Must select question to broadcast.', 400);
+  io.emit('broadcastQuestion', question);
 });
 
 let questionData = {
   a: 0,
-  b:0,
-  c:0,
-  d:0
+  b: 0,
+  c: 0,
+  d: 0
 };
 
-var answerData = {
+const answerChartData = {
   labels: ['A', 'B', 'C', 'D'],
   datasets: [
     {
@@ -203,50 +209,50 @@ var answerData = {
   ]
 };
 
-//listen for question reponses, send back to front end to trigger render that shows answers
-io.on('connection', (socket)=>{
-  socket.on('answerData', (answer)=>{
-    console.log('student answer received, data: ', answer);
-    switch(answer){
-      case 'A': answerData.datasets[0].data[0] += 1;
-        break;
-      case 'B': answerData.datasets[0].data[1] += 1;
-        break;
-      case 'C': answerData.datasets[0].data[2] += 1;
-        break;
-      case 'D': answerData.datasets[0].data[3] += 1;
-        break;
-      default: console.error('Question answer data not sent/stored properly.');
-    };
-    console.log('questionData after increment: ');
-    console.log(answerData);
+server.post('answer-chart-data', (req, res, next) => {
+  const answer = req.body.answer;
+  if (!answer) return next(new ServerError('Must provide answer data.', 400))
+  switch (answer) {
+    case 'A': answerChartData.datasets[0].data[0] += 1;
+      break;
+    case 'B': answerChartData.datasets[0].data[1] += 1;
+      break;
+    case 'C': answerChartData.datasets[0].data[2] += 1;
+      break;
+    case 'D': answerChartData.datasets[0].data[3] += 1;
+      break;
+    default: console.error('Question answer data not sent/stored properly.');
+  };
 
-    io.emit('answer', answerData);
-  })
+  io.emit('answerChartData', answerChartData);
+})
+
+server.post('add-question-queue', (req, req, next) => {
+  const { question, studentUsername, id } = req.body;
+  if (!question) return new ServerError('Must provide question.', 400);
+  if (!studentUsername) return new ServerError('Must provide student username.', 400);
+  if (!id) return new ServerError('Must provide student ID.', 400);
+
+  io.emit('updateStudentQuestions', question, studentUsername, id);
 });
 
-//listen for QQ additions
-io.on('connection', (socket)=>{
-  socket.on('QQadd', (question, studentUsername, id)=>{
-    console.log('backend QQadd socket reached. question: ', question, studentUsername, id);
-    io.emit('Qsend', question, studentUsername, id);
-  });
+server.post('delete-question-queue', (req, res, next) => {
+  const { questionID } = req.body;
+  if (!questionID) return next(new ServerError('Must provide question ID.', 400));
+
+  io.emit('deleteQuestionQueue', questionID);
 });
 
-//listen for QQ deletions
-io.on('connection', (socket)=>{
-  socket.on('QQdelete', (questionID)=>{
-    console.log('backend QQdelete socket reached. question: ', questionID);
-    io.emit('deleteQQ', questionID);
-  });
+server.post('reset-chart-data', (req, res, next) => {
+  answerChartData.datasets[0].data = [0, 0, 0, 0];
 });
 
-io.on('connection', (socket) => {
-  socket.on('resetAnswerData', () => {
-    answerData.datasets[0].data = [0, 0, 0, 0];
-  })
+server.use('/*', (req, res, next) => {
+  next(new ServerError(`Cannot ${req.method} ${req.originalUrl}.`, 404));
 });
+
+server.use(errorHandler);
 
 http.listen(3001, () => {
-  console.log('Node server listening on port 3001 successfully.')
+  console.log('Server listening on port 3001 successfully.');
 });
